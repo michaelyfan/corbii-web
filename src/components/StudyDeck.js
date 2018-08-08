151573c7fc7f73da7b6b32fb02a3c039bb2d078d
Sarah Weyand <sarahmackenzie015@gmail.com>
2018-08-08 16:09:17 -0400

    implement hotkeys

--- src/components/StudyDeck.js
+++ src/components/StudyDeck.js
@@ -1,13 +1,68 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { getDeckForStudy, getUserProfileInfo, updateCardPersonalData, updateCardPersonalDataLearner } from '../utils/api';
 import { shiftInArray } from '../utils/tools';
 import routes from '../routes/routes';
 import queryString from 'query-string';
 import { Link, NavLink, withRouter } from 'react-router-dom';
+import {HotKeys} from 'react-hotkeys';
 
 class NewCardOptions extends React.Component {
+    constructor(props) {
+    super(props);
+
+    this.state = {
+      active: false,
+    };
+  }
+
+   setActive(state) {
+    this.setState({ active: true });
+    this.refs.option.focus();
+  }
+
+  componentDidMount() {
+    this.focusDiv();
+  }
+
+  componentDidUpdate() {
+    if(this.state.active) {
+      this.focusDiv();
+    }
+  }
+
+  focusDiv() {
+    ReactDOM.findDOMNode(this.refs.option).focus();
+  }
+
   render() {
+    const newKeyMap = {
+      'one': '1',
+      'two': '2',
+      'three': '3',
+    }
+
+    let newHandlers;
+    if (lastSelectedQuality == 0) {
+      newHandlers = {
+        'one': (event) => {this.props.submitCard(0, true)},
+        'two': (event) => {this.props.submitCard(1, true)}
+      }
+    } else if (lastSelectedQuality == 1) {
+      newHandlers = {
+        'one': (event) => {this.props.submitCard(0, true)},
+        'two': (event) => {this.props.submitCard(1, true)},
+        'three': (event) => {this.props.submitCard(2, true)}
+      }
+    } else {
+      newHandlers = {
+        'one': (event) => {this.props.submitCard(0, true)},
+        'two': (event) => {this.props.submitCard(1, true)},
+        'three': (event) => {this.props.submitCard(3, true)}
+      }
+    }
+      
     const lastSelectedQuality = this.props.card ? this.props.card.lastSelectedQuality : null;
 
     let options;
@@ -50,32 +105,53 @@ class NewCardOptions extends React.Component {
     }
 
     return (
-      <div>
-        {options}
-      </div>
+      <HotKeys keyMap = {newKeyMap} handlers = {newHandlers}>
+        <div className = 'option-menu center-button'  ref='option'>
+          {options}
+        </div>
+      </HotKeys>
     )
   }
 }
 
 class NotNewCardOptions extends React.Component {
   render() {
+    const oldKeyMap = {
+      'one': '1',
+      'two': '2',
+      'three': '3',
+      'four': '4',
+      'five': '5',
+      'six': '6',
+    }
+
+    const oldHandlers = {
+      'one': (event) => {this.props.submitCard(0, true)},
+      'two': (event) => {this.props.submitCard(0, true)},
+      'three': (event) => {this.props.submitCard(0, true)},
+      'four': (event) => {this.props.submitCard(3, false)},
+      'five': (event) => {this.props.submitCard(4, false)},
+      'six': (event) => {this.props.submitCard(5, false)}
+    }
+
     return (
-      <div>
-        <p className = 'rating-prompt' id = 'rating-question'> on a scale of one to six, how comfortable are you with this card?</p>
-        <p className = 'rating-prompt'> one = very uncomfortable &nbsp; &nbsp; six = very comfortable</p>
-        <div className = 'rating-buttons'>
-          <button className = 'accuracy-button maroon number-scale' onClick={() => {this.props.submitCard(0, true)}}>1</button>
-          <button className = 'accuracy-button red number-scale' onClick={() => {this.props.submitCard(0, true)}}>2</button>
-          <button className = 'accuracy-button orange number-scale' onClick={() => {this.props.submitCard(0, true)}}>3</button>
-          <button className = 'accuracy-button yellow number-scale' onClick={() => {this.props.submitCard(3, false)}}>4</button>
-          <button className = 'accuracy-button lime number-scale' onClick={() => {this.props.submitCard(4, false)}}>5</button>
-          <button className = 'accuracy-button green number-scale' onClick={() => {this.props.submitCard(5, false)}}>6</button>
+      <HotKeys keyMap = {oldKeyMap} handlers = {oldHandlers}>
+        <div>
+          <p className = 'rating-prompt' id = 'rating-question'> on a scale of one to six, how comfortable are you with this card?</p>
+          <p className = 'rating-prompt'> one = very uncomfortable &nbsp; &nbsp; six = very comfortable</p>
+          <div className = 'rating-buttons'>
+            <button className = 'accuracy-button maroon number-scale' onClick={() => {this.props.submitCard(0, true)}}>1</button>
+            <button className = 'accuracy-button red number-scale' onClick={() => {this.props.submitCard(0, true)}}>2</button>
+            <button className = 'accuracy-button orange number-scale' onClick={() => {this.props.submitCard(0, true)}}>3</button>
+            <button className = 'accuracy-button yellow number-scale' onClick={() => {this.props.submitCard(3, false)}}>4</button>
+            <button className = 'accuracy-button lime number-scale' onClick={() => {this.props.submitCard(4, false)}}>5</button>
+            <button className = 'accuracy-button green number-scale' onClick={() => {this.props.submitCard(5, false)}}>6</button>
+          </div>
         </div>
-      </div>
+      </HotKeys>
     )
   }
 }
-
 
 class StudyCard extends React.Component {
   constructor(props) {
@@ -114,6 +190,15 @@ class StudyCard extends React.Component {
 
   render() {
     const { cardData, card } = this.props;
+
+    const flipKeyMap = {
+      'flip-card': 'space'
+    }
+
+    const flipHandlers = {
+      'flip-card': (event) => {this.flip()}
+    }
+
     return (
       <div>
         <div className = 'study-card'>        
@@ -137,14 +222,16 @@ class StudyCard extends React.Component {
                       : <NotNewCardOptions submitCard={this.submitCard} />
                   }
                 </div>
-              : <div className = 'center-button'>
-                  <button 
-                    className = 'primary-button' 
-                    id = 'flip-card' 
-                    onClick={this.flip}>
-                      flip card
-                  </button>
-                </div>
+              : <HotKeys keyMap = {flipKeyMap} handlers = {flipHandlers}>
+                  <div className = 'center-button'>
+                    <button 
+                      className = 'primary-button' 
+                      id = 'flip-card' 
+                      onClick={this.flip}>
+                        flip card
+                    </button>
+                  </div>
+                </HotKeys>
           }
         </div>
       </div>
