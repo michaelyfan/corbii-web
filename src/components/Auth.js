51df25fef708061066a2ac83e7dcc5f13c0dee30
Michael Fan <michaelyfan100@gmail.com>
2021-08-22 12:10:32 -0700

    Cleanup after rebase

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import * as firebaseui from 'firebaseui';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from '../utils/firebase';
 
